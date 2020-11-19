f70659f0a037868cb97cdfa565818f4e71b8be1e
Nichlas Overgaard Laugesen <nl@hldata.dk>
2020-11-19 13:32:21 +0100

    Lot's of changes

--- firstGame.py
+++ firstGame.py
@@ -6,15 +6,15 @@ win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("First Game")
 
 # This goes outside the while loop, near the top of the program
-walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
-walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
-bg = pygame.image.load('bg.jpg')
-char = pygame.image.load('standing.png')
+walkRight = [pygame.image.load('Images/R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('Images/L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('Images/bg.jpg')
+char = pygame.image.load('Images/standing.png')
 
 x = 50
 y = 425
-width = 40
-height = 60
+width = 64
+height = 64
 vel = 5
 isJump = False
 jumpCount = 10
@@ -22,7 +22,14 @@ left = False
 right = False
 walkCount = 0
 
+def redrawGameWindow():
+    global walkCount
 
+    win.blit(bg, (0,0))
+    pygame.draw.rect(win,(255,0,0), (x, y, width, height))
+    pygame.display.update()
+
+#mainloop
 run = True
 while run:
     pygame.time.delay(50)
@@ -50,8 +57,5 @@ while run:
         else:
             isJump = False
             jumpCount  = 10
-    win.fill((0,0,0))
-    pygame.draw.rect(win,(255,0,0), (x, y, width, height))
-    pygame.display.update()
 
 pygame.quit()
