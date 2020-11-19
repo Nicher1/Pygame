71c8224cd737eb4e38a29c8867ec0f68f84665fc
Nichlas Overgaard Laugesen <nl@hldata.dk>
2020-11-19 13:11:28 +0100

    transform to platformer + adding import of images
    transform to platformer + adding import of images

--- firstGame.py
+++ firstGame.py
@@ -5,14 +5,23 @@ win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("First Game")
 
+# This goes outside the while loop, near the top of the program
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+
 x = 50
 y = 425
 width = 40
 height = 60
 vel = 5
-
 isJump = False
 jumpCount = 10
+left = False
+right = False
+walkCount = 0
+
 
 run = True
 while run:
@@ -29,10 +38,6 @@ while run:
     if keys[pygame.K_RIGHT] and x < 500 - width - vel:
         x += vel
     if not(isJump):
-        if keys[pygame.K_UP] and y > vel:
-            y -= vel
-        if keys[pygame.K_DOWN] and y < 500 - height - vel :
-            y += vel
         if keys[pygame.K_SPACE]: #Jumping function with quadratic equation
             isJump = True
     else:
